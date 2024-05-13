896836dfdc6c825c1cc5138ee634909b836264bd
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-13 15:59:51 +0300

    initial commit

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,16 +8,33 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey,
       body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Image.asset(
-                'lib/images/imageslogo.png',
-                height: 240,
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Image.asset(
+                  'lib/images/imageslogo.png',
+                  height: 240,
+                ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 48,
+              ),
+              Text(
+                'Just Do It',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Text(
+                'Brand new sneakers and custom kicks made with premium quality ',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
