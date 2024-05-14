f61711dd1ed68079a876a172e0583d49d870aff8
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-14 13:58:09 +0300

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_e_commerce/pages/home_page.dart';
 import 'package:sneaker_e_commerce/pages/intro_page.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: IntroPage(),
+      home: HomePage(),
     );
   }
 }
