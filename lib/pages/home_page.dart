e479a5cbacd680746f99b0215e5b7e9fb94bcdf9
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-15 11:47:27 +0300

    modified

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sneaker_e_commerce/components/bottom_nav_bar.dart';
+import 'package:sneaker_e_commerce/pages/cart_page.dart';
+import 'package:sneaker_e_commerce/pages/shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +19,14 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  final List<Widget> _pages = [
+    //shop page
+    const ShopPage(),
+
+    //cartpage
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +34,7 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      body: _pages[_selectedIndex],
     );
   }
 }
