bf835f93040583042fc7c947c0bc4b86afb4d151
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-16 11:32:35 +0300

    initial commit

---
+++ lib/models/shoe.dart
@@ -0,0 +1,12 @@
+class Shoe {
+  final String name;
+  final String price;
+  final String ImagePath;
+  final String description;
+
+  Shoe(
+      {required this.ImagePath,
+      required this.description,
+      required this.name,
+      required this.price});
+}
