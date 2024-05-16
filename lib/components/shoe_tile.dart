8a4b714d9113b5874b4110fc704f4562d6b00430
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-16 11:32:08 +0300

    initial commit

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_e_commerce/models/shoe.dart';
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+
+  ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+}
