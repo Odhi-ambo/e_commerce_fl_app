78a4d977782401bf3d33f45cd6a4444298b1a762
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-17 09:58:57 +0300

    modified

--- lib/models/shoe.dart
+++ lib/models/shoe.dart
@@ -1,12 +1,12 @@
 class Shoe {
   final String name;
   final String price;
-  final String ImagePath;
+  final String imagePath;
   final String description;
 
   Shoe(
-      {required this.ImagePath,
-      required this.description,
-      required this.name,
-      required this.price});
+      {required this.name,
+      required this.price,
+      required this.imagePath,
+      required this.description});
 }
